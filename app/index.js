74179389b6d600e17c8278e05d9a7df190ae42b8
gurami152 <gurami152@gmail.com>
2021-01-31 21:53:35 +0200

    initial server commit

---
+++ app/index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("./models");
+const Role = db.role;
+
+const app = express();
+
+let corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// // parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//routes
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to my diploma API" });
+});
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
+
+// on dev
+db.sequelize.sync({force: true}).then(() => {
+    console.log('Drop and Resync Db');
+    initial();
+});
+// on prod
+// db.sequelize.sync()
+
+function initial() {
+    Role.create({
+        id: 1,
+        name: "user"
+    });
+
+    Role.create({
+        id: 2,
+        name: "moderator"
+    });
+
+    Role.create({
+        id: 3,
+        name: "admin"
+    });
+}
